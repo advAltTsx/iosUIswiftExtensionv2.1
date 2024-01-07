79f34d78666dab94a666c46a11d4d4ef385d4dc2
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2024-01-07 10:04:19 +0530

    Update carousel.tsx

--- iosadv/components/ui/carousel.tsx
+++ iosadv/components/ui/carousel.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons"
 import useEmblaCarousel, {
-  type EmblaCarouselType as CarouselApi,
+  type UseEmblaCarouselType as CarouselApi,
   type EmblaOptionsType as CarouselOptions,
   type EmblaPluginType as CarouselPlugin,
 } from "embla-carousel-react"
